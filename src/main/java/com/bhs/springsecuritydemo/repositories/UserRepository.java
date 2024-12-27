0f84fc71688b3bb77e0bfcac916a7c000c55a1e3
Hema Sai Bonda <hemasaibonda1@gmail.com>
2024-12-27 16:16:18 +0530

    Added UserRepository for auth: findUserByEmail

---
+++ src/main/java/com/bhs/springsecuritydemo/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.bhs.springsecuritydemo.repositories;
+
+import com.bhs.springsecuritydemo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+
+    Optional<User> findUserByEmail(String email);
+    
+}
