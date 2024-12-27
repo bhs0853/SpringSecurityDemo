d23b797612f3c17265113d5dc40a598651590033
Hema Sai Bonda <hemasaibonda1@gmail.com>
2024-12-27 16:12:47 +0530

    Added User model implementing the UserDetails for Auth

---
+++ src/main/java/com/bhs/springsecuritydemo/models/User.java
@@ -0,0 +1,61 @@
+package com.bhs.springsecuritydemo.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, name = "user_id")
+    private String userId;
+
+    @Column(nullable = false,name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false, nullable = false, name = "created_at")
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, name = "updated_at")
+    private Date updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    
+}
